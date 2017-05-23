20be84e865c4ea33e91a552667f14956f79198a1
FrankLikuohao <frank.likuohao@gmail.com>
2017-05-23 22:16:49 +0800

    新增類別範例

---
+++ Lecture01-Examples/Example03/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student someone = new Student();
+            someone.name = "Thomas";
+            Console.WriteLine("學生姓名:{0}",
+                someone.name);
+        }
+    }
+}
