df9b25d2f7da677707ca836b54aa3575b4fe041f
FrankLikuohao <frank.likuohao@gmail.com>
2017-05-23 20:53:45 +0800

    範例1修正"

--- Lecture01-Examples/Example01/Program.cs
+++ Lecture01-Examples/Example01/Program.cs
@@ -11,6 +11,7 @@ namespace Example01
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!!");
+            Console.WriteLine("這是一個基本輸入輸出的範例");
         }
     }
 }
